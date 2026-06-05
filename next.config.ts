b66e74be321181831211d971709779c079c1be44
Ashish Gupta <ashish.gupta.sion@gmail.com>
2026-06-05 14:03:58 +0000

    added cicd and deploy scrip

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Outputs a static 'out' folder during next build
+  images: {
+    unoptimized: true, // Required if using the Next.js Image component statically
+  },
 };
 
 export default nextConfig;
